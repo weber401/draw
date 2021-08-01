ad853b3db64d735d6cef43a5dd150a3546016e11
weber401 <dipendramahat581@gmail.com>
2021-08-01 22:00:26 +0545

    nothing

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
 alert('hello new here');
 
+element.addEventListener('click', function(e) {
+    console.log('This element has been clicked!');
+    console.log(e.target.innerHTML);
+    // render_emails('56');
+});
+
  
