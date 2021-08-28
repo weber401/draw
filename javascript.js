1778e9cab3759df1cfb7426aa40167373361cb97
weber401 <dipendramahat581@gmail.com>
2021-08-28 09:20:48 +0545

    some debug stuff

--- javascript.js
+++ javascript.js
@@ -7,6 +7,11 @@ let drawing = false;
 let x = 0;
 let y = 0;
 
+//jsut for some debugging
+let mousedown = false;
+let mousemove = false;
+let mouseup = false;
+
 
 // boolean check for different tools
 let pencil = true;  //for now drawing bool works fine
@@ -41,6 +46,8 @@ const context = mycanvas.getContext('2d');
 console.log("what");
 
 mycanvas.addEventListener('mousedown', e => {
+    document.querySelector('#status-mouse-up').innerHTML = "mouse-up: false";
+    document.querySelector('#status-mouse-down').innerHTML = "mousedown: true";
     x = e.offsetX;
     y = e.offsetY;
     console.log("mousedown");
@@ -51,6 +58,7 @@ mycanvas.addEventListener('mousedown', e => {
 });
 
 mycanvas.addEventListener('mousemove', e => {
+    document.querySelector('#status-mouse-move').innerHTML = "mousemove: True";
     if (drawing === true) {
         drawline(context, x, y, e.offsetX, e.offsetY);
         x = e.offsetX;
@@ -59,6 +67,8 @@ mycanvas.addEventListener('mousemove', e => {
 });
 
 mycanvas.addEventListener('mouseup', e => {
+    document.querySelector('#status-mouse-down').innerHTML = "mouse-down: false";
+    document.querySelector('#status-mouse-up').innerHTML = "mouse-up: true";
     if (drawing === true) {
         drawline(context, x, y, e.offsetX, e.offsetY);
         x = 0;
@@ -83,6 +93,7 @@ function drawline(context, x1, y1, x2, y2) {
 document.querySelector('#clear').addEventListener('click', clearcanvas);
 
 //this is just to make sure things are all right
+// funciton to clear canvas
 function clearcanvas() {
     context.clearRect(0, 0, mycanvas.width, mycanvas.height);
 }
